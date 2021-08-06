0f089268cf65ec70b76fc672698bb64818630d2f
Nathan Rothenberger <nroth10@gmail.com>
2021-08-06 08:34:36 -0500

    convert existing plugin to run within the new menu context

--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -7,52 +7,56 @@ import { generateLocalTextStylesDoc } from "./textStyles";
 // You can access browser APIs in the <script> tag inside "ui.html" which has a
 // full browser environment (see documentation).
 
-// This shows the HTML page in "ui.html".
-figma.showUI(__html__);
-figma.ui.resize(400, 300);
+if (figma.command === "CONFIG") {
+  console.log("CONFIG");
 
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
-figma.ui.onmessage = (msg) => {
-  if (msg.type.includes("CANCEL")) {
-    figma.closePlugin();
-    return;
-  }
+  // This shows the HTML page in "ui.html".
+  figma.showUI(__html__);
+  figma.ui.resize(400, 300);
 
-  const mainFrame = getStoredFrame("MainFrame") as FrameNode;
-  applyMainFrameStyles(mainFrame);
-
-  for (let index = 0; index < msg.type.length; index++) {
-    const element = msg.type[index];
-
-    switch (element) {
-      case "CREATE_COLOR_STYLES":
-        console.log("create color styles");
-        generateLocalPaintStylesDoc(mainFrame);
-        break;
-
-      case "CREATE_EFFECT_STYLES":
-        console.log("create effect styles");
-        generateLocalEffectStylesDoc(mainFrame);
-        break;
-
-      case "CREATE_TEXT_STYLES":
-        console.log("create text styles");
-        generateLocalTextStylesDoc(mainFrame);
-        break;
-
-      default:
-        break;
+  // Calls to "parent.postMessage" from within the HTML page will trigger this
+  // callback. The callback will be passed the "pluginMessage" property of the
+  // posted message.
+  figma.ui.onmessage = (msg) => {
+    if (msg.type.includes("CANCEL")) {
+      figma.closePlugin();
+      return;
     }
-  }
 
-  positionMainFrame(mainFrame);
+    const mainFrame = getStoredFrame("MainFrame") as FrameNode;
+    applyMainFrameStyles(mainFrame);
 
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
-};
+    for (let index = 0; index < msg.type.length; index++) {
+      const element = msg.type[index];
+
+      switch (element) {
+        case "CREATE_COLOR_STYLES":
+          console.log("create color styles");
+          generateLocalPaintStylesDoc(mainFrame);
+          break;
+
+        case "CREATE_EFFECT_STYLES":
+          console.log("create effect styles");
+          generateLocalEffectStylesDoc(mainFrame);
+          break;
+
+        case "CREATE_TEXT_STYLES":
+          console.log("create text styles");
+          generateLocalTextStylesDoc(mainFrame);
+          break;
+
+        default:
+          break;
+      }
+    }
+
+    positionMainFrame(mainFrame);
+
+    // Make sure to close the plugin when you're done. Otherwise the plugin will
+    // keep running, which shows the cancel button at the bottom of the screen.
+    figma.closePlugin();
+  };
+}
 
 // Global logs
 // console.log("console", console);
