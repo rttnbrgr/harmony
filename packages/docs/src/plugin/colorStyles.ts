2e5ff28e3db588b3cfdbe5857775581dba17b0af
Nathan Rothenberger <nroth10@gmail.com>
2021-09-05 11:41:27 -0500

    extract get insert position

--- packages/docs/src/plugin/colorStyles.ts
+++ packages/docs/src/plugin/colorStyles.ts
@@ -219,6 +219,23 @@ function buildSample(paintStyle: PaintStyle) {
   return sampleFrame;
 }
 
+function getColorStylesFrameInsertPosition(mainFrame: FrameNode) {
+  // Check if textStyles frame exists
+  // This feels brittle
+  // Should probably save a value on the frame and lookup that up
+  const textStylesFrameExists = mainFrame.findChildren((x) => {
+    // console.log("child", x);
+    // console.log("child name", x.name);
+    return x.name === "Text Styles";
+  }).length;
+
+  // Based on this, set insert position
+  // Either at the beginning or after the text styles
+  const insertPosition = textStylesFrameExists ? 1 : 0;
+  // console.log("insertPosition", insertPosition);
+  return insertPosition;
+}
+
 async function generateLocalPaintStylesDoc(mainFrame: FrameNode) {
   await figma.loadFontAsync({ family: "Roboto", style: "Regular" });
 
@@ -239,21 +256,10 @@ async function generateLocalPaintStylesDoc(mainFrame: FrameNode) {
   // Build the style frames and append them to the master artboard
   buildStyleFrames<PaintStyle>(localPaintStyles, paintStylesMasterFrame, buildSample, { x: 64 + 16, y: null });
 
-  // Check if textStyles frame exists
-  // This feels brittle
-  const textStylesFrameExists = mainFrame.findChildren((x) => {
-    // console.log("child", x);
-    // console.log("child name", x.name);
-    return x.name === "Text Styles";
-  }).length;
-  // console.log("textStylesFrameExists", textStylesFrameExists);
-
-  // Based on this, set insert position
-  const insertPosition = textStylesFrameExists ? 1 : 0;
-  // console.log("insertPosition", insertPosition);
+  // Get insert position
+  const insertPosition = getColorStylesFrameInsertPosition(mainFrame);
 
   // Add style frame to main frame
-  // Either at the beginning or after the text styles
   mainFrame.insertChild(insertPosition, paintStylesMasterFrame);
 }
 
