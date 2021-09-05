779dbcd739031951c5099e239f9b4f7dea5d60f5
Nathan Rothenberger <nroth10@gmail.com>
2021-09-05 18:18:48 -0500

    cleanup color styles

--- packages/docs/src/plugin/colorStyles.ts
+++ packages/docs/src/plugin/colorStyles.ts
@@ -1,39 +1,5 @@
 import { addHeaderToFrame, applyStyleFrameStyles, buildStyleFrames, getStoredFrame } from "./helpers";
-import {
-  buildComponentStyleSwatch,
-  buildStyleFramesNew,
-  createColorStyleDocBlockInstance,
-  DOC_BLOCK_ROOT,
-  DOC_BLOCK_SWATCH,
-} from "./new";
-import { getOpacityStringFromSolidPaint, getSpecStringFromColorStop, gradiantReducer } from "./getSpec";
-import { deriveRgbValue } from "./utils";
-// Take value between 0 - 1 and get an rgb
-const deriveRgbValue = (val: number) => Math.round(val * 255);
-
-export function isInt(n: number) {
-  return n % 1 === 0;
-}
-
-// get a string from a given paint color
-const getRgbStringFromLocalStyle = (style) => {
-  // limit to single fill
-  const rgbObject = style.paints[0].color;
-  const r = deriveRgbValue(rgbObject.r);
-  const g = deriveRgbValue(rgbObject.g);
-  const b = deriveRgbValue(rgbObject.b);
-  return `RGB: [${r}, ${g}, ${b}]`;
-};
-
-// take a style, return a specString
-function buildPaintStyleSpecString(style: PaintStyle): string {
-  let specString;
-  specString = style.name;
-  specString += " - ";
-  specString += getRgbStringFromLocalStyle(style);
-  console.log(specString);
-  return specString;
-}
+import { buildComponentStyleSwatch, buildStyleFramesNew, createColorStyleDocBlockInstance } from "./new";
 
 type textOptions = {
   x: number;
@@ -51,124 +17,6 @@ function addText(string: string = "Your new text", options: textOptions): TextNo
   }
   figma.currentPage.appendChild(newText);
   return newText;
-}
-
-// Takes a paint style and returns a frame documenting that style
-// function buildSample(paintStyle: PaintStyle = samplePaintStyle) {
-function buildSample(paintStyle: PaintStyle) {
-  console.log("build sample");
-  if (!paintStyle) {
-    return;
-  }
-  const paintStyleName = paintStyle.name;
-  const paintStyleId = paintStyle.id;
-  let paintStyleSpec = "";
-
-  // console.log("🎨 ", paintStyleName);
-  // console.log(paintStyle);
-
-  /*
-   * Logic to build spec string
-   */
-
-  // safety checking
-  let isSolid = true;
-  let isSingleFill = true;
-  if (paintStyle.paints.length > 1) {
-    isSingleFill = false;
-    paintStyleSpec = "Multiple Fills";
-  }
-
-  let firstPaint = paintStyle.paints[0];
-
-  if (isSingleFill && firstPaint.type !== "SOLID") {
-    isSolid = false;
-
-    // For image filles?
-    if (firstPaint.type === "IMAGE") {
-      paintStyleSpec = `Image fill`;
-    }
-
-    // For Gradient fills
-    // "GRADIENT_LINEAR" | "GRADIENT_RADIAL" | "GRADIENT_ANGULAR" | "GRADIENT_DIAMOND"
-    if (firstPaint.type !== "IMAGE") {
-      // let thisPaintStyle = paintStyle.paints[0];
-      // console.log("firstPaint", firstPaint);
-      // thisPaintStyle.gradientStops.reduce()
-      const gradiantStopsString = firstPaint.gradientStops.map(getSpecStringFromColorStop).reduce(gradiantReducer);
-      // console.log("gradiantStopsString", gradiantStopsString);
-
-      paintStyleSpec = `RGBA: ${gradiantStopsString}`;
-    }
-  }
-  if (isSingleFill && isSolid && "color" in firstPaint) {
-    // get color portion of spec
-    let specStringTest = getRgbStringFromLocalStyle(paintStyle);
-    // get opacity portion of spec
-    let opacitySpecString = getOpacityStringFromSolidPaint(firstPaint);
-    // Stitch teh spec string together
-    if (opacitySpecString) {
-      specStringTest += " @ ";
-      specStringTest += opacitySpecString;
-    }
-
-    // console.log("specStringTest", specStringTest);
-    paintStyleSpec = specStringTest;
-  }
-
-  // put it here
-  const sampleX = 400;
-  const sampleY = 0;
-  const spacer = 8;
-  const rectSize = spacer * 8;
-  const textX = sampleX + rectSize + spacer;
-
-  // build the rect
-  const colorStyleRect = figma.createRectangle();
-  colorStyleRect.x = sampleX;
-  colorStyleRect.y = sampleY;
-  colorStyleRect.fillStyleId = paintStyleId;
-  colorStyleRect.resize(rectSize, rectSize);
-  colorStyleRect.cornerRadius = spacer;
-  figma.currentPage.appendChild(colorStyleRect);
-
-  // Build title
-  const colorStyleTitleText = addText(paintStyleName, {
-    x: textX,
-    y: sampleY,
-  });
-  // Build spec
-  const colorStyleSpecText = addText(paintStyleSpec, {
-    x: textX,
-    y: sampleY + 14,
-  });
-
-  // Group text nodes
-  const textGroup = figma.group([colorStyleTitleText, colorStyleSpecText], figma.currentPage);
-
-  // Selection testing
-  // const newNodes: SceneNode[] = [textGroup, colorStyleRect];
-  // newNodes.push(textGroup);
-  // newNodes.push(colorStyleRect);
-  // figma.currentPage.selection = newNodes;
-  // console.log("selection", figma.currentPage.selection);
-  // console.log("figma", figma);
-
-  // Create the frame, append text + rect, position it
-  const sampleFrame = figma.createFrame();
-  sampleFrame.appendChild(colorStyleRect);
-  sampleFrame.appendChild(textGroup);
-  sampleFrame.layoutMode = "HORIZONTAL";
-  sampleFrame.itemSpacing = 8;
-  sampleFrame.counterAxisAlignItems = "CENTER";
-  sampleFrame.x = sampleX;
-  let getSampleFrameWidth = () => sampleFrame.width;
-  let sampleFrameWidth = getSampleFrameWidth();
-
-  sampleFrame.resizeWithoutConstraints(sampleFrameWidth, rectSize);
-  // console.log("sampleFrame", sampleFrame);
-
-  return sampleFrame;
 }
 
 function getColorStylesFrameInsertPosition(mainFrame: FrameNode) {
@@ -216,14 +64,7 @@ async function generateLocalPaintStylesDoc(mainFrame: FrameNode) {
   mainFrame.insertChild(insertPosition, paintStylesMasterFrame);
 }
 
-export {
-  deriveRgbValue,
-  getRgbStringFromLocalStyle,
-  buildPaintStyleSpecString,
-  buildSample,
-  generateLocalPaintStylesDoc,
-  addText,
-};
+export { generateLocalPaintStylesDoc, addText };
 
 // Multi fill
 // opacity on solid paint fill
