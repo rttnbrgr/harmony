82be1a27ba751c4a8a40b796af26d778357cf6c2
Nathan Rothenberger <nroth10@gmail.com>
2021-10-09 09:36:35 -0500

    clean effect style

--- packages/docs/src/plugin/effectStyles.ts
+++ packages/docs/src/plugin/effectStyles.ts
@@ -1,78 +1,7 @@
-import { addText } from "./utils";
-import { addHeaderToFrame, buildStyleFrames, getStoredFrame } from "./frameHelpers";
+import { addHeaderToFrame } from "./frameHelpers";
 import { boostrapStyleDocFrame } from "./styleDocFrame";
-import { getSpecStringFromEffectStyle } from "./getSpec";
-
-// Takes a paint style and returns a frame documenting that style
-// function buildSample(paintStyle: PaintStyle = samplePaintStyle) {
-function buildSample(effectStyle: EffectStyle) {
-  if (!effectStyle) {
-    return;
-  }
-
-  const effectStyleName = effectStyle.name;
-  const effectStyleId = effectStyle.id;
-  let effectStyleSpec = getSpecStringFromEffectStyle(effectStyle);
-
-  console.log("🎨 ", effectStyleName, effectStyle);
-
-  // put it here
-  const sampleX = 400;
-  const sampleY = 0;
-  const spacer = 8;
-  const rectSize = spacer * 8;
-  const textX = sampleX + rectSize + spacer;
-
-  const whiteFill: Array<SolidPaint> = [
-    {
-      type: "SOLID",
-      color: {
-        r: 1,
-        g: 1,
-        b: 1,
-      },
-    },
-  ];
-
-  // build the rect
-  const colorStyleRect = figma.createRectangle();
-  colorStyleRect.x = sampleX;
-  colorStyleRect.y = sampleY;
-  colorStyleRect.fills = whiteFill;
-  colorStyleRect.effectStyleId = effectStyleId;
-  colorStyleRect.resize(rectSize, rectSize);
-  colorStyleRect.cornerRadius = spacer;
-  figma.currentPage.appendChild(colorStyleRect);
-
-  // Build title
-  const colorStyleTitleText = addText(effectStyleName);
-  colorStyleTitleText.x = textX;
-  colorStyleTitleText.y = sampleY;
-
-  // Build spec
-  const colorStyleSpecText = addText(effectStyleSpec);
-  colorStyleSpecText.x = textX;
-  colorStyleSpecText.y = sampleY + 14;
-
-  // Group text nodes
-  const textGroup = figma.group([colorStyleTitleText, colorStyleSpecText], figma.currentPage);
-
-  // Create the frame, append text + rect, position it
-  const sampleFrame = figma.createFrame();
-  sampleFrame.appendChild(colorStyleRect);
-  sampleFrame.appendChild(textGroup);
-  sampleFrame.layoutMode = "HORIZONTAL";
-  sampleFrame.itemSpacing = 8;
-  sampleFrame.counterAxisAlignItems = "CENTER";
-  sampleFrame.x = sampleX;
-  let getSampleFrameWidth = () => sampleFrame.width;
-  let sampleFrameWidth = getSampleFrameWidth();
-
-  sampleFrame.resizeWithoutConstraints(sampleFrameWidth, rectSize);
-  // console.log("sampleFrame", sampleFrame);
-
-  return sampleFrame;
-}
+import { buildStyleFramesNew } from "./new";
+import { createEffectStyleDocBlockInstance } from "./docBlockInstance";
 
 async function generateLocalEffectStylesDoc(mainFrame: FrameNode) {
   await figma.loadFontAsync({ family: "Roboto", style: "Regular" });
@@ -86,8 +15,11 @@ async function generateLocalEffectStylesDoc(mainFrame: FrameNode) {
   // Add header
   addHeaderToFrame("Effect Styles", effectStylesMasterFrame);
 
-  // Build the style frames and append them to the master artboard
-  buildStyleFrames<EffectStyle>(localEffectStyles, effectStylesMasterFrame, buildSample, { x: 64 + 16, y: null });
+  // Testing New --------------------------------------------------------------------------------------------
+  // this boostraps the component swatch. this needs work
+  // getComponentStyleSwatch();
+  // this builds the style frame
+  buildStyleFramesNew<EffectStyle>(localEffectStyles, effectStylesMasterFrame, createEffectStyleDocBlockInstance);
 
   // Add style frame to main frame
   mainFrame.appendChild(effectStylesMasterFrame);
