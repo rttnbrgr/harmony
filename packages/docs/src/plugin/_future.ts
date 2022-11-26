fc5878471d7dcac30d9e7d2bb2e5e28b4cdfc052
Nathan Rothenberger <nroth10@gmail.com>
2022-11-26 13:35:29 -0600

    todo: add new todos

--- packages/docs/src/plugin/_future.ts
+++ packages/docs/src/plugin/_future.ts
@@ -2,3 +2,12 @@
 // get all colors on the page
 // print them with a hex value
 // check which ones match a layer style
+
+/* color style todos */
+
+// Multi fill
+// opacity on solid paint fill
+// Additional metadata (gradiant type)
+// different color modes
+// Better visual
+// more info on image fill
