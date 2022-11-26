9ea9faa8bf67f97287e70a1de32ec08907846e4c
Nathan Rothenberger <nroth10@gmail.com>
2022-11-26 17:40:17 -0600

    remove new plugin

--- packages/new-plugin/src/app/components/form/types.ts
+++
@@ -1,3 +0,0 @@
-export type Actions = "CREATE_COLOR_STYLES" | "CREATE_EFFECT_STYLES" | "CREATE_TEXT_STYLES";
-
-export type ReducerState = { [key in Actions]: boolean };
--- packages/new-plugin/src/plugin/_future.ts
+++
@@ -1,4 +0,0 @@
-// Future Feat: Parse whats on the page/doc
-// get all colors on the page
-// print them with a hex value
-// check which ones match a layer style
