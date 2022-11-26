d0001ddc2ccacefe0e02d1c6faac7100422a039e
Nathan Rothenberger <nroth10@gmail.com>
2022-11-26 17:41:29 -0600

    feat: remove new plugin

--- packages/new-plugin/src/app/components/form/constants.ts
+++
@@ -1,7 +0,0 @@
-import { Actions } from "./types";
-
-export const FORM_FIELD_METADATA: { id: Actions; label: string }[] = [
-  { id: "CREATE_COLOR_STYLES", label: "Color Styles" },
-  { id: "CREATE_EFFECT_STYLES", label: "Effect Styles" },
-  { id: "CREATE_TEXT_STYLES", label: "Text Styles" },
-];
