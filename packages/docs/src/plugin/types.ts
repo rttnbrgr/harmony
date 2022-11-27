7ae9b163a36ff8844cf1e6b91ae9d3a394ea1a5d
Nathan Rothenberger <nroth10@gmail.com>
2022-11-27 08:50:31 -0600

    add new types for doc block work

--- packages/docs/src/plugin/types.ts
+++ packages/docs/src/plugin/types.ts
@@ -3,7 +3,36 @@ export const TEXT_STYLES_FRAME = "TextStylesFrame";
 
 export type FigmaDocsFrame = "ColorStylesFrame" | "TextStylesFrame" | "EffectStylesFrame" | "MainFrame";
 
+// Refactor as enums
+// export enum DocsFrames {
+//   ColorStyles = "ColorStylesFrame",
+//   TextStyles = "TextStylesFrame",
+//   EffectStyles = "EffectStylesFrame",
+//   Main = "MainFrame",
+// }
+
 export type textOptions = {
   x: number;
   y: number;
 };
+
+// For the swatch doc block
+export const DOC_BLOCK_ROOT: DocBlockRootNodes = "DocBlockComponent";
+export const DOC_BLOCK_SWATCH: DocBlockNodes = "DocBlockSwatch";
+export const DOC_BLOCK_TITLE: DocBlockNodes = "DocBlockTitle";
+export const DOC_BLOCK_SPEC: DocBlockNodes = "DocBlockSpec";
+
+// For the text doc block
+export const DOC_BLOCK_2_ROOT: DocBlockRootNodes = "DocBlockComponent2";
+export const DOC_BLOCK_2_TITLE: DocBlockNodes = "DocBlockTitle2";
+export const DOC_BLOCK_2_SPEC: DocBlockNodes = "DocBlockSpec2";
+
+export type DocBlockRootNodes = "DocBlockComponent" | "DocBlockComponent2";
+
+export type DocBlockNodes =
+  | DocBlockRootNodes
+  | "DocBlockSwatch"
+  | "DocBlockTitle"
+  | "DocBlockSpec"
+  | "DocBlockTitle2"
+  | "DocBlockSpec2";
